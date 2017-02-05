941ce9c0bf429bbb3c274253489403ccdaec844d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-02-05 17:21:19 +0100

    Back to basics. For now

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-const bunyan = require('bunyan')
-const bunyanFormat = require('bunyan-format')
 const createServer = require('./server')
 const config = require('./config.json')
 
-const logger = bunyan.createLogger({
-  name: config.appname,
-  level: config.log.level,
-  stream: bunyanFormat({
-    outputMode: config.log.format
-  })
-})
-
-const server = createServer({
-  config: config,
-  logger: logger
-})
+const server = createServer({ config })
 
 server.listen(config.port, () => {
-  logger.info('Service started listing on port ' + config.port)
+  console.log('Start listening on port ' + config.port)
 })
--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const express = require('express')
 const StandardError = require('standard-error')
-const expressBunyanLogger = require('express-bunyan-logger')
+const morgan = require('morgan')
 const cors = require('cors')
-const loggerProperties = require('./lib/middlewares/logger')
-const formatError = require('./lib/middlewares/formatError')
 const caf = require('./src')
 const fs = require('fs')
 
 function createServer ({ config, logger }) {
   const app = express()
   app.set('json spaces', 2)
-  app.set('logger', logger)
   app.disable('x-powered-by')
 
   app.use(cors())
-
-  app.use(expressBunyanLogger({
-    name: 'requests',
-    logger: logger,
-    excludes: loggerProperties.excludes,
-    includesFn: loggerProperties.includesFn,
-    format: '":remote-address :incoming :method HTTP/:http-version :status-code :res-headers[content-length] :referer :user-agent[family] :user-agent[major].:user-agent[minor] :user-agent[os] :response-time ms";'
-  }))
-
-  app.use((req, res, next) => {
-    req.logger = logger
-    next()
-  })
+  app.use(morgan('dev'))
 
   app.use('/api', caf({
     serviceParams: {
@@ -44,7 +29,13 @@ function createServer ({ config, logger }) {
     next(new StandardError('no route for URL ' + req.url, {code: 404}))
   })
 
-  app.use(formatError)
+  app.use(function (err, req, res, next) {
+    console.error(err)
+    res.status(500).send({
+      code: 500,
+      message: err.message
+    })
+  })
 
   return app
 }
