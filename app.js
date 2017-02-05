82fef47b3c77502b5169e3e0b0de61ec39dbcbff
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-02-05 18:16:26 +0100

    Merge app and server

--- app.js
+++
@@ -1,8 +0,0 @@
-const createServer = require('./server')
-const config = require('./config.json')
-
-const server = createServer({ config })
-
-server.listen(config.port, () => {
-  console.log('Start listening on port ' + config.port)
-})
--- server.js
+++ server.js
@@ -4,40 +4,41 @@ const morgan = require('morgan')
 const cors = require('cors')
 const cafRouter = require('./src/router')
 const fs = require('fs')
+const config = require('./config.json')
 
-function createServer ({ config, logger }) {
-  const app = express()
-  app.set('json spaces', 2)
-  app.disable('x-powered-by')
+const port = config.port || 3000
+const app = express()
 
-  app.use(cors())
-  app.use(morgan('dev'))
+app.set('json spaces', 2)
+app.disable('x-powered-by')
 
-  app.use('/api', cafRouter({
-    serviceParams: {
-      host: config.cafHost,
-      cert: fs.readFileSync(config.cafSslCertificate),
-      key: fs.readFileSync(config.cafSslKey)
-    },
-    pingParams: {
-      codePostal: config.codePostal,
-      numeroAllocataire: config.numeroAllocataire
-    }
-  }))
+app.use(cors())
+app.use(morgan('dev'))
 
-  app.use(function notFound (req, res, next) {
-    next(new StandardError('no route for URL ' + req.url, {code: 404}))
+app.use('/api', cafRouter({
+  serviceParams: {
+    host: config.cafHost,
+    cert: fs.readFileSync(config.cafSslCertificate),
+    key: fs.readFileSync(config.cafSslKey)
+  },
+  pingParams: {
+    codePostal: config.codePostal,
+    numeroAllocataire: config.numeroAllocataire
+  }
+}))
+
+app.use(function notFound (req, res, next) {
+  next(new StandardError('no route for URL ' + req.url, {code: 404}))
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err)
+  res.status(500).send({
+    code: 500,
+    message: err.message
   })
+})
 
-  app.use(function (err, req, res, next) {
-    console.error(err)
-    res.status(500).send({
-      code: 500,
-      message: err.message
-    })
-  })
-
-  return app
-}
-
-module.exports = createServer
+app.listen(port, () => {
+  console.log('Start listening on port ' + port)
+})
